9162ffe2c6efe5aaf76bd835dbdf342e3353109d
rolls982 <53308009+rolls982@users.noreply.github.com>
2020-02-21 15:25:30 +0000

    swap peeing hub and spoke gateway values flags

--- main.tf
+++ main.tf
@@ -40,8 +40,8 @@ resource "azurerm_virtual_network_peering" "vnet_peer_1" {
   remote_virtual_network_id    = data.azurerm_virtual_network.spokevnet.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
-  allow_gateway_transit        = false
-  use_remote_gateways          = true
+  allow_gateway_transit        = true
+  use_remote_gateways          = false
 }
 
 resource "azurerm_virtual_network_peering" "vnet_peer_2" {
@@ -52,8 +52,8 @@ resource "azurerm_virtual_network_peering" "vnet_peer_2" {
   remote_virtual_network_id    = data.azurerm_virtual_network.hubvnet.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
-  allow_gateway_transit        = true
-  use_remote_gateways          = false
+  allow_gateway_transit        = false
+  use_remote_gateways          = true
 }
 
 
