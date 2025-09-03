59decf3010e1c30e1ea4da6dea3f10bfde23dbf3
Shaun Bresnahan <ashaun@ukho.gov.uk>
2025-09-03 11:08:29 +0100

    bit of refactor

--- peering.tf
+++ peering.tf
@@ -1,25 +1,3 @@
-data "azurerm_resource_group" "hub" {
-  provider = azurerm.hub
-  name     = var.hubrg
-}
-
-data "azurerm_resource_group" "spoke" {
-  provider = azurerm.spoke
-  name     = var.spokerg
-}
-
-data "azurerm_virtual_network" "hubvnet" {
-provider                       = azurerm.hub
-name                           = var.hubvnet
-resource_group_name            = data.azurerm_resource_group.hub.name
-}
-
-data "azurerm_virtual_network" "spokevnet" {
-  provider                     = azurerm.spoke
-  name                         = var.spokevnet
-  resource_group_name          = data.azurerm_resource_group.spoke.name
-}
-
 resource "azurerm_virtual_network_peering" "vnet_peer_1" {
   provider                     = azurerm.hub
   name                         = var.peer1to2
